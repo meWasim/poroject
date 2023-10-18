f914de7d0e769497908de29d890d5ad340df621c
Wasim Ansari <matainja0120@gmail.com>
2023-10-18 15:14:15 +0530

    main

---
+++ resources/views/components/nav-bar.blade.php
@@ -0,0 +1,44 @@
+<nav class="navbar navbar-expand-lg navbar-light fixed-top mask-custom shadow-0">
+  <div class="container">
+    <a class="navbar-brand" href="#!"><span style="color: #5e9693;">DANCE</span><span style="color: #219ebc;">CLUB</span></a>
+    <button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
+      data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+      aria-label="Toggle navigation">
+      <i class="fas fa-bars"></i>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#!">ABOUT ME</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#!">WORK</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#!">BLOG</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#!">CONTACT</a>
+        </li>
+      </ul>
+    </div>
+    <!-- Move icons to the right -->
+    <ul class="navbar-nav ms-auto">
+      <li class="nav-item me-3 me-lg-0">
+        <a class="nav-link" href="#!">
+          <i class="bi bi-shopping-cart"></i>
+        </a>
+      </li>
+      <li class="nav-item me-3 me-lg-0">
+        <a class="nav-link" href="#!">
+          <i class="bi bi-twitter"></i>
+        </a>
+      </li>
+      <li class="nav-item me-3 me-lg-0">
+        <a class="nav-link" href="#!">
+          <i class="bi bi-instagram"></i>
+        </a>
+      </li>
+    </ul>
+  </div>
+</nav>
